8632914ac3eccb532afb2bd227335dc8206aad08
leedaham <mydaham1004@gmail.com>
2019-11-11 17:06:32 +0900

    2019/11/11 upload

---
+++ ch02/js/calc.js
@@ -0,0 +1,17 @@
+//js파일을 만듦으로서 라이브러리화 시키는 것
+function plus( x, y ){
+    var z = x + y;
+    return z;
+}
+function minus( x, y ){
+    var z = x - y;
+    return z;
+}
+function multi( x, y ){
+    var z = x * y;
+    return z;
+}
+function div( x, y ){
+    var z = x / y;
+    return z;
+}
